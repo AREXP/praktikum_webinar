8968d84aac7840e676d304224535106b5e94913d
AREXP <arexp19@gmail.com>
2020-06-28 11:54:54 +0300

    add counting

--- script.py
+++ script.py
@@ -1,8 +1,13 @@
 def main():
     fn = 'text'
+    count = 0
     with open(fn) as filestream:
         for line in filestream:
             print(line)
+            count += 1
+
+    print('-' * 10)
+    print(f'Total lines count: {count}')
 
 
 if __name__ == '__main__':
